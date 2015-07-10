cd305606bc5ea4fce7607edf1de3c77cb7e1b979
Anjali bhawnani <anjali.bhawnani@optimusinfo.com>
2015-07-10 18:40:12 +0530

    UPDATE

--- assignments/SQLQuery3.sql
+++ assignments/SQLQuery3.sql
@@ -42,11 +42,3 @@ INSERT INTO t_transaction VALUES('U3','P2',CONVERT(DATE,'26-11-2010',105),'Order
 INSERT INTO t_transaction VALUES('U2','P1',CONVERT(DATE,'15-12-2010',105),'Order',75.0);
 INSERT INTO t_transaction VALUES('U3','P2',CONVERT(DATE,'15-01-2011',105),'Payment',250.0);
 GO
---Create required table format
-SELECT t_user_master.User_Name,t_product_master.Product_Name,
-SUM(CASE WHEN Transaction_Type='order' THEN Transaction_Amount ELSE 0.0 END) AS Ordered_Quantity,
-SUM(CASE WHEN Transaction_Type='Payment' THEN Transaction_Amount ELSE 0.0 END) AS Amount_Paid,
-MAX(Transaction_Date) AS Last_Transaction_Date,
-SUM(CASE WHEN Transaction_Type='order' THEN Transaction_Amount ELSE 0.0 END)*Cost_Per_Item-SUM(CASE WHEN Transaction_Type='Payment' THEN Transaction_Amount ELSE 0.0 END) AS Balance
-FROM ((t_product_master INNER JOIN t_transaction ON t_product_master.Product_ID=t_transaction.Product_ID) INNER JOIN t_user_master ON t_user_master.User_ID=t_transaction.User_ID) GROUP BY User_Name,Product_Name,Cost_Per_Item;
-
